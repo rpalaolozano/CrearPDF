fde6049b0b6cd4f09c7528d0914c3115c81af2b8
Raul <r.palaolozano@edu.gva.es>
2023-11-21 12:31:02 +0100

    Añado el proyecto

---
+++ crearpdf/CrearPDF.java
@@ -0,0 +1,26 @@
+package crearpdf;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Paragraph;
+import java.io.FileOutputStream;
+
+public class CrearPDF {
+    public void añadirTexto() throws Exception {
+        Document document = new Document();
+        String parrafo = "Lentejas = 5€";
+        PdfWriter.getInstance(document, new FileOutputStream("ticket_restaurante.pdf"));
+        document.open();
+        document.add(new Paragraph(parrafo));
+        document.close();
+    }
+
+    public static void main(String args[]) {
+        try {
+            CrearPDF p = new CrearPDF();
+            p.añadirTexto();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
